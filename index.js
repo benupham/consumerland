4f4cdbdb1da455d22cbbd44b1cf5f9b5c9a8549a
Ben Upham <bcupham@gmail.com>
2018-04-21 14:58:39 -0700

    in progress..

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ import Icon from 'ol/style/icon';
 import Style from 'ol/style/style';
 import Stroke from 'ol/style/stroke';
 import Extent from 'ol/extent';
+import GeoJSON from 'ol/format/geojson';
+import Select from 'ol/interaction/select';
+import CanvasMap from 'ol/canvasmap';
+import Overlay from 'ol/overlay';
+import Control from 'ol/control/control';
 
 
 
@@ -51,6 +56,8 @@ for (let i = 0; i < featureCount; ++i) {
   })
  
   feature.set('originalStyle',featureStyle);
+  feature.setId('beans-' + i);
+  feature.set('price', 5.99);
   feature.setStyle(
     function (resolution) {
       this.get('originalStyle').getImage().setScale(1 / resolution);
@@ -60,22 +67,32 @@ for (let i = 0; i < featureCount; ++i) {
   features[i] = feature;
 }
 
-let center = new Point([0,0]);
-let boundaryBox = new Polygon.fromExtent([-80*200,80*200,80*200,-80*200]);
-let strokeStyle = new Style({
-  stroke: new Stroke({
-    color: '#319FD3',
-    width: 1
-  }),
-});
-const theCenter = new Feature(center);
-const theBoundaryBox = new Feature(boundaryBox);
+// let center = new Point([0,0]);
+// let boundaryBox = new Polygon.fromExtent([-80*200,80*200,80*200,-80*200]);
+// let strokeStyle = new Style({
+//   stroke: new Stroke({
+//     color: '#319FD3',
+//     width: 1
+//   }),
+// });
+// const theCenter = new Feature(center);
+// const theBoundaryBox = new Feature(boundaryBox);
 
-features.push(theCenter,theBoundaryBox);
+//features.push(theCenter,theBoundaryBox);
 
-var vectorSource = new VectorSource({
+const vectorSource = new VectorSource({
   features: features
 });
+
+const jsonVectorSource = new VectorSource({
+       url: './sample.geojson',
+       format: new GeoJSON()
+       //features: (new GeoJSON()).readFeatures(geojsonObject)
+});
+
+const jsonVectorLayer = new VectorLayer({
+  source: jsonVectorSource
+})
 
 
 var vectorLayer = new VectorLayer({
@@ -96,11 +113,10 @@ var view = new View({
   })
 })
 
-console.log(view.getProjection());
 
 var map = new olMap({
-  renderer: /** @type {Array<ol.renderer.Type>} */ (['webgl', 'canvas']),
-  layers: [vectorLayer],
+  renderer: /** @type {Array<ol.renderer.Type>} */ (['canvas']),
+  layers: [vectorLayer, jsonVectorLayer],
   target: document.getElementById('map'),
   view: view
 });
@@ -113,8 +129,103 @@ map.on('click', function(evt) {
   '<br> Pixel from Coordinate: ' +  map.getPixelFromCoordinate(evt.coordinate) +
   '<br> event pixel: ' + map.getEventPixel(evt) + 
   '<br> event coordinate: ' + map.getEventCoordinate(evt.coordinate) + 
-  '<br> features at pixel: ' + map.getFeaturesAtPixel(evt.coordinate, {hitTolerance: 100}) +
+  '<br> features at pixel: ' + map.getFeaturesAtPixel(evt.pixel) +
   '<br> Resolution: ' + map.getView().getResolution() + 
   '<br> Zoom: ' + map.getView().getZoom();
-  console.log(map.getView());
+  //console.log(map.getView());
+  //console.log(map.getFeaturesAtPixel(evt.pixel, {hitTolerance: 100})[0].getProperties().name);
+  //console.log(features[100]);
+  //console.log(evt);
 });
+
+
+let highlight; 
+
+const onProductHover = function(evt) {
+  const pixel = evt.pixel;
+  const feature = map.forEachFeatureAtPixel(pixel, (feature) => feature);
+
+  if (feature !== highlight) {
+    if (highlight) {
+      highlight.setStyle( function (resolution) {
+        this.get('originalStyle').getImage().setScale(1 / resolution);
+        return this.get('originalStyle');
+      });
+    }
+    if (feature) {
+      feature.setStyle( function (resolution) {
+        this.get('originalStyle').getImage().setScale(1 / resolution * 1.1);
+        return this.get('originalStyle');
+      }); 
+    }
+    highlight = feature;
+  }
+
+}
+
+map.on('pointermove', onProductHover);
+
+let clicked; 
+
+const onProductClick = function(evt) {
+  const pixel = evt.pixel;
+  const feature = map.forEachFeatureAtPixel(pixel, (feature) => feature);
+  console.log('step 1' + feature, clicked);
+  if (feature !== clicked) {
+    console.log('step 2: ' + feature,clicked);
+    if (clicked) {
+      clicked.setStyle( function (resolution) {
+        this.get('originalStyle').getImage().setScale(1 / resolution);
+        this.get('originalStyle').setZIndex(0);
+        return this.get('originalStyle');
+      });
+    }
+    if (feature) {
+      feature.setStyle( function (resolution) {
+        this.get('originalStyle').getImage().setScale(1);
+        this.get('originalStyle').setZIndex(1);
+        return this.get('originalStyle');
+      }); 
+    }
+    clicked = feature;
+  }
+}
+
+map.on('click', onProductClick);
+
+/* Search field prototype */
+
+const searchForm = document.getElementById('search');
+
+const handleSearch = function(e) {
+  e.preventDefault();
+  const query = searchForm.elements[0].value;
+  if (query != '') {
+    try {
+      const feature = vectorLayer.getSource().getFeatureById(query); 
+      map.getView().animate({
+        center: feature.getGeometry().getCoordinates(),
+        zoom: 6
+      })
+    }
+    catch(err) {
+      console.log(query +' not found');
+    }
+
+  }
+
+
+
+//  map.getLayers().getSource().getFeatureById()
+
+} 
+searchForm.addEventListener('submit', handleSearch);
+
+
+const search = new Control({
+  element: document.getElementById('pm-search-wrapper'),
+
+
+});
+map.addControl(search);
+
